351eaa71db331a75c0afe1abafe15b7d8d6bdfa4
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-02-02 18:46:30 +0300

    Implement Pagination With Query Parameter

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,40 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get()
-    findAll() {
-        return "This action returns all the coffees"
-    }
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees.Limit: ${limit}, Offset: ${offset}`;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return `This action returns #${id} coffee`;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
 
-    @Post()
-    create(@Body() body) {
-        return body;
-    }
+  @Post()
+  create(@Body() body) {
+    return body;
+    // return `This action creates a coffee`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
+  }
 }
