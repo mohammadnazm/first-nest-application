d5d8c6f28e2ba8cb5baf3e989c82c360aa303ea9
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-02-10 23:51:33 +0300

    Add Request Logging With Middleware

--- src/common/middleware/loggin/logging.middleware.ts
+++ src/common/middleware/loggin/logging.middleware.ts
@@ -3,7 +3,10 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    console.log('Hi From Middleware');
+    console.time('Request-response time');
+    console.log('Hi from middleware!');
+
+    res.on('finish', () => console.timeEnd('Request-response time'));
     next();
   }
 }
